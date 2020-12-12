9a1a714e785930bf11dd1812f5d9d3bdab97f018
MeanBachine <david.flyr@gmail.com>
2020-12-12 04:18:05 -0600

    Add start/end screens
    Put some really good art in to be the start and end screens of the game

---
+++ Flight Risk/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    [SerializeField] string _level;
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(_level);
+    }
+}
